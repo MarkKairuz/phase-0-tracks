88508def725f5e7b3ee347984643414157f43ed7
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-03 23:29:47 -0700

    Complete Interview with the Vampire questionaire.

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+Name = gets.chomp
+puts "How old are you?"
+Age = gets.chomp
+puts "What year were you born?"
+Year_Born = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+Garlic_Bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+Health_Insurance = gets.chomp
