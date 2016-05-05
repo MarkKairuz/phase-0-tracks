87133792b2625712b3eb139438de8f5dea4b97d2
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-04 17:28:20 -0700

    Create vampire detection logic.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,3 +8,14 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 Garlic_Bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 Health_Insurance = gets.chomp
+
+if ( 2016 - Year_Born == Age ) && ( Garlic_Bread || Health_Insurance )
+	puts "Probably not a vampire."
+elsif !( 2016 - Year_Born == Age ) && ( !Garlic_Bread || !Health_Insurance )
+	puts "Probably a vampire."
+elsif !( 2016 - Year_Born == Age ) && ( !Garlic_Bread && !Health_Insurance )
+	puts "Almost certainly a vampire."
+elsif ( Name == "Drake Cula" ) || ( Name == "Tu Fang" )
+	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
