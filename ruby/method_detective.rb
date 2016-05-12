7a8960675dd08f91213b295a3a30086530250c7f
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-11 23:28:22 -0700

    Finish method_detective methods.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -28,12 +28,12 @@
 "The mystery of the missing first letter".slice!(1..38)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
