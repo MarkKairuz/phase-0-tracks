bf173893d0e922b49ca554d5bbbf495a3e6b6c50
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-12 19:37:56 -0700

    Document methods.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -5,35 +5,35 @@
 # committed the crime, add additional calls to prove it.
 
 "iNvEsTiGaTiOn".swapcase
-# => “InVeStIgAtIoN”
+# swapcase("iNvEsTiGaTiOn") => “InVeStIgAtIoN”
 
 "zom".insert( 1, "o" )
-# => “zoom”
+# insert("zom", 1, "o") => “zoom”
 
 "enhance".center(15)
-# => "    enhance    "
+# center("enhance", 15) => "    enhance    "
 
 "Stop! You’re under arrest!".upcase
-# => "STOP! YOU’RE UNDER ARREST!"
+# upcase("Stop! You’re under arrest!") => "STOP! YOU’RE UNDER ARREST!"
 
 "the usual".ljust(18, " suspects")
-# => "the usual suspects"
+# ljust("the usual", 18, " suspects") => "the usual suspects"
 
 " suspects".rjust(18, "the usual")
-# => "the usual suspects"
+# rjust(" suspects", 18, "the usual") => "the usual suspects"
 
 "The case of the disappearing last letter".chop
-# => "The case of the disappearing last lette"
+# chop("The case of the disappearing last letter") => "The case of the disappearing last lette"
 
 "The mystery of the missing first letter".slice!(1..38)
-# => "he mystery of the missing first letter"
+# slice("The mystery of the missing first letter", 1..38) => "he mystery of the missing first letter"
 
 "Elementary,    my   dear        Watson!".squeeze
-# => "Elementary, my dear Watson!"
+# squeeze(Elementary,    my   dear        Watson!) => "Elementary, my dear Watson!"
 
 "z".ord
-# => 122 
+# ord("z") => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
 "How many times does the letter 'a' appear in this string?".count "a"
-# => 4
+# count("How many times does the letter 'a' appear in this string?", "a") => 4
