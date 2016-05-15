41607f609ed6093956da6c5fc2c1f1fc41b2ae34
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-14 22:45:58 -0700

    Fix fertilizers error and add comments.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,11 +1,18 @@
+# Highways nested data structures
+# by Mark Kairuz
+
+# Nested highway 405 system
 Highway_405 = {
+    # mini van in highway 405
     mini_van: {
         type: 'Dodge Grand Caravan',
+        # mini van info
         car_info: {
             color: 'red',
             engine: 'V6',
             fuel: 'gas'
         },
+        # passengers and sports equipment
         contains: [
             "Kelly", 
             "Clark",
@@ -14,26 +21,32 @@ Highway_405 = {
             "soccer balls"
         ]
     },
+    # Sports car in highway 405
     sports_car: {
         type: 'Chevrolet Camaro coupe',
+        # Sports car info
         car_info: {
             color: 'maroon',
             engine: 'V8',
             fuel: 'gas'
         },
+        # One passenger
         contains: [
             "Blake"
         ]
     },
+    # Delivery truck in highway 405
     delivery_truck: {
         type: 'Ford E-350 Econoline',
+        # Delivery truck info
         car_info: {
             color: 'white',
             engine: 'V8',
             fuel: 'diesel'
         },
+        # Passenger with gardening equipment
         contains: [
-            "Ahzar"
+            "Ahzar",
             "fertilizers"
         ]
     }
