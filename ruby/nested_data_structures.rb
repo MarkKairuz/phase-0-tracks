84412bcd3664a1204db5a6a7876e7a9a255f7995
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-14 22:42:22 -0700

    Create nested highway system with some comments.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,41 @@
+Highway_405 = {
+    mini_van: {
+        type: 'Dodge Grand Caravan',
+        car_info: {
+            color: 'red',
+            engine: 'V6',
+            fuel: 'gas'
+        },
+        contains: [
+            "Kelly", 
+            "Clark",
+            "Susan",
+            "Mary",
+            "soccer balls"
+        ]
+    },
+    sports_car: {
+        type: 'Chevrolet Camaro coupe',
+        car_info: {
+            color: 'maroon',
+            engine: 'V8',
+            fuel: 'gas'
+        },
+        contains: [
+            "Blake"
+        ]
+    },
+    delivery_truck: {
+        type: 'Ford E-350 Econoline',
+        car_info: {
+            color: 'white',
+            engine: 'V8',
+            fuel: 'diesel'
+        },
+        contains: [
+            "Ahzar"
+            "fertilizers"
+        ]
+    }
+}
+
