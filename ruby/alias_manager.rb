732cc5fb46017b4e953626995c86702dd7b57355
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-15 02:45:01 -0700

    Attempt to finish the code.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,5 +1,11 @@
+# Alias Manager
 # By Mark Kairuz
+# Note: this name generator can code a that that has 1 to 3 sections.
+# 	For Example: Billy Bob Joe.
+# 	Also, keep note of your name because there is a self-destruct sequence in the end.
 
+# Declare and empty hash
+name_hash = {}
 # Request for a name or quit
 puts "Type in a name or type 'quit' to exit."
 name = gets.chomp
@@ -16,12 +22,19 @@ while name.downcase != 'quit' do
 
 	# Swap the split first and last name
 	name_swap.reverse!
-	p name_swap.length
 	name_swap[0].capitalize! # Capitalize the new first name.
-	name_swap[1].capitalize! # Capitalize the new last name.
+	if name_swap.length >= 2 # Capitalize new 2nd name if it exists.
+		name_swap[1].capitalize! # Capitalize the new secone name.
+		if name_swap.length >= 3 # Capitalize new 3rd name if it exists.
+			name_swap[2].capitalize! # Capitalize the new third name.
+		end
+	end
 
 	# Combine the swapped split first and last name
 	new_name = name_swap.join(' ')
+
+	# Add to the hash of names
+	name_hash[name] = new_name
 
 	# Print the new name
 	p "Your code name is now #{new_name}."
@@ -33,6 +46,12 @@ while name.downcase != 'quit' do
 	name = gets.chomp
 end
 
-puts "Good luck, Agent! This message will self distruct in 10 seconds"
+# 
+#number_of_names = name_hash.legth
+#number_of_names.times { |x| puts name_hash}
+
+
+puts "" # blank line
+puts "Good luck, Agent! This message will self-destruct in 10 seconds"
 sleep(10) # 10 seconds pause.
 puts "\e[2J\e[f" # Code for clearing the screen.
