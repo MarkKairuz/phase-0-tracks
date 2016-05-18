b850c87ab804295dd488b5264043663b0f7424d6
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-18 00:16:08 -0700

    Add class, characteristics and behavior comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# CLASS ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Eyelash length: varies
+# Friendly: yes
+# Floppy ears: varies
+# Name: varies
+# Fur color: varies
+# Estimated lifespan: 25
+
+# BEHAVIOR -----------------------
+# Jump
+# Roll
+# Play dead
