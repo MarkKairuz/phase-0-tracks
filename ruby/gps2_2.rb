7db2ca950d29ad5fdd84bae61a603ac5c9aed80d
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-20 00:47:03 -0700

    Finalize the project without the final comments.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,3 +1,34 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Use .split to break up each word in the string
+  # Convert array into a hash
+  # Create an empty hash
+  # Shovel the information from the array into the hash
+  # set default quantity to 1
+  # print the list to the console [can you use one of your other methods here?]
+# output: [hash]
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: create a hash to store input
+# output: the hash 
+
+# Method to remove an item from the list
+# input: item name to remove
+# steps: method to delete key-value from hash
+# output: the hash with the item removed
+
+# Method to replace the quantity of an item
+# input: item name and quantity to update
+# steps: method to replace quantity of item
+# output: the hash with the quantity updated
+
+# Method to print a list and make it look pretty
+# input: the hash itself
+# steps: use iteration to print sentences of items
+# output: pretty sentences that state item and quantity to purchase
+
 puts "What items do you want to purchase?"
 items_to_purchase = gets.chomp
 
@@ -13,11 +44,11 @@ end
  	add_item = gets.chomp
  	quantity = 1
  	while add_item != "done"
- 		puts "How many of this item do you want to add to the list?"
- 		quantity = gets.chomp
- 		add_list[add_item] = quantity
- 		puts "What would you like to add to the list? Type 'done' when you are finished."
- 		add_item = gets.chomp
+ 	puts "How many of this item do you want to add to the list?"
+ 	quantity = gets.chomp
+ 	add_list[add_item] = quantity
+ 	puts "What would you like to add to the list? Type 'done' when you are finished."
+ 	add_item = gets.chomp
  	end
  	p add_list
  end
@@ -26,24 +57,24 @@ end
  	puts "What would you like to delete? Type 'done' when you are finished."
  	del_item = gets.chomp
  	while del_item != "done"
-		delete_item.delete(del_item)
-		puts "What would you like to delete? Type 'done' when you are finished."
-		del_item = gets.chomp
-	end
-	p delete_item
+ 	delete_item.delete(del_item)
+ 	puts "What would you like to delete? Type 'done' when you are finished."
+ 	del_item = gets.chomp
+ end
+ 	p delete_item
 end
 
 def number_change(num_change)
-	puts "What would you like to change? Type 'done' when you are finished."
-	change = gets.chomp
-	while change != "done"
-		puts "What is the updated quantity for #{change}?"
-		updated_info = gets.chomp
- 		num_change[change] = updated_info
- 		puts "What would you like to change? Type 'done' when you are finished."
-		change = gets.chomp
-	end
-	p num_change
+ puts "What would you like to change? Type 'done' when you are finished."
+ change = gets.chomp
+ while change != "done"
+ 	puts "What is the updated quantity for #{change}?"
+ 	updated_info = gets.chomp
+ 	num_change[change] = updated_info
+ 	puts "What would you like to change? Type 'done' when you are finished."
+ 	change = gets.chomp
+ end
+ p num_change
 end
 
 
@@ -51,3 +82,7 @@ list1 = create_list(items_to_purchase)
 list2 = item_add(list1)
 list3 = item_delete(list2)
 list4 = number_change(list3)
+
+puts "**************************************************"
+puts "Updated Grocery List"
+list4.each {|key, data| puts "#{key}: #{data}"}
