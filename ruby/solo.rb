b2f3f44ce908f919a8c397c70d7dd5da7a0502db
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-21 20:12:49 -0700

    Initiate pseudocode.

---
+++ ruby/solo.rb
@@ -0,0 +1,53 @@
+# Mark Kairuz
+# GPS 2.2 with Maria
+
+# Droid class
+
+# Initiate
+
+# Attribute 1: Communication
+
+# Attribute 2: Droid Type
+
+# Attribute 3: Color
+
+# Attribute 4: Mobility
+
+# Method 1: Common phrase
+
+# Method 2: Common action
+
+# Method 3: Recharge?
+
+
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Use .split to break up each word in the string
+  # Convert array into a hash
+  # Create an empty hash
+  # Shovel the information from the array into the hash
+  # set default quantity to 1
+  # print the list to the console [can you use one of your other methods here?]
+# output: [hash]
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: create a hash to store input
+# output: the hash 
+
+# Method to remove an item from the list
+# input: item name to remove
+# steps: method to delete key-value from hash
+# output: the hash with the item removed
+
+# Method to replace the quantity of an item
+# input: item name and quantity to update
+# steps: method to replace quantity of item
+# output: the hash with the quantity updated
+
+# Method to print a list and make it look pretty
+# input: the hash itself
+# steps: use iteration to print sentences of items
+# output: pretty sentence
