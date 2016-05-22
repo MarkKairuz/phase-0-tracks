50064d86ae7a317b785dc9ef1ed4f2f7b20c4e18
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-21 20:29:43 -0700

    Update pseudocode.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -6,48 +6,39 @@
 # Initiate
 
 # Attribute 1: Communication
+	# Input: Initialized array = Beeps and bloops
+	# steps: Store communication Beeps and bloops in array
+	# output: array
 
 # Attribute 2: Droid Type
+	# Input: array from driver
+	# steps: converts type. Defaults as Astromech Droid.
+	# output: array
 
 # Attribute 3: Color
+	# Input: Initialized array = silver
+	# steps: Stores as no paint.
+	# output: array
 
 # Attribute 4: Mobility
+	# Input: array from driver
+	# steps: Converts mobility. Defaults as immobile.
+	# output: array
 
 # Method 1: Common phrase
+	# Input: Initialized array = silence
+	# steps: Stores as silent.
+	# output: array
 
 # Method 2: Common action
+	# Input: Initialized array = stays put
+	# steps: Stores no action.
+	# output: array
 
 # Method 3: Recharge?
+	# Input: array from driver
+	# steps: Charge 10% or below gets recharged.
+		# Charge above notifies that charge is ok.
+	# output: array
 
 
-
-# Method to create a list
-# input: string of items separated by spaces (example: "carrots apples cereal pizza")
-# steps: 
-  # Use .split to break up each word in the string
-  # Convert array into a hash
-  # Create an empty hash
-  # Shovel the information from the array into the hash
-  # set default quantity to 1
-  # print the list to the console [can you use one of your other methods here?]
-# output: [hash]
-
-# Method to add an item to a list
-# input: item name and optional quantity
-# steps: create a hash to store input
-# output: the hash 
-
-# Method to remove an item from the list
-# input: item name to remove
-# steps: method to delete key-value from hash
-# output: the hash with the item removed
-
-# Method to replace the quantity of an item
-# input: item name and quantity to update
-# steps: method to replace quantity of item
-# output: the hash with the quantity updated
-
-# Method to print a list and make it look pretty
-# input: the hash itself
-# steps: use iteration to print sentences of items
-# output: pretty sentence
