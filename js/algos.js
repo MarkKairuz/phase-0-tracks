748b77e73aedd749b3b37e1853c8a75ba511768e
Mark Kairuz <mark.kairuz@gmail.com>
2016-05-28 21:49:50 -0700

    Find Key-Value match initiate with functions.

--- js/algos.js
+++ js/algos.js
@@ -28,6 +28,26 @@ var longest_string = "";
 	return longest_string;
 }
 
+
+function Key_Value_Match(object1, object2) {
+
+var key_value_pair = false;
+key_count = Object.keys(object1).length;
+	for (var i = 0; i < key_count; i++) {
+		key = Object.keys(object1)[i];
+		if (object1[key] == object2[key]) {
+			key_value_pair = true;
+		}
+	}
+return key_value_pair;
+}
+
+
+
+
+
+
+// test the functions
 phrase_array = ["long phrase","longest phrase","longer phrase"];
 console.log("The longest phrase of [" + phrase_array + "] is: " + Longest_Phrase(phrase_array) + ".");
 
@@ -36,3 +56,13 @@ console.log("The longest phrase of [" + phrase_array2 + "] is: " + Longest_Phras
 
 phrase_array3 = ["one too many","two of a king","three is a crowd"];
 console.log("The longest phrase of [" + phrase_array3 + "] is: " + Longest_Phrase(phrase_array3) + ".");
+
+
+first_object = {name: "Steven", age: 54};
+second_object = {name: "Tamir", age: 54};
+console.log("Do these have similar key-value set?: ");
+console.log(first_object);
+console.log(second_object);
+console.log("Answer: " + Key_Value_Match(first_object, second_object));
+
+
